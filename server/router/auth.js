34dcf26eafd74a63c2319f1e162daa2fb2c46d27
asfandyar-sabri <69463140+asfandyar-sabri@users.noreply.github.com>
2023-01-13 23:20:08 +0500

    Now changed promises to async await to add data in db, signing off for the day

--- server/router/auth.js
+++ server/router/auth.js
@@ -8,39 +8,80 @@ router.get('/', (req, res) => {
     res.send("<h1>Hello from auth</h1>");
 })
 
-router.post('/register', (req, res) => {
+
+// promises
+// router.post('/register', async (req, res) => {
+
+//     const { name, email, phone, work, password, cpassword } = req.body;
+
+//     if(!name || !email || !phone || !work || !password || !cpassword)
+//         return res.status(422).json({
+//             error: "Please fill all fields properly"
+//         })
+
+//     user.findOne({
+//         "email": email
+//     }).then((userExists) => {
+//         if(userExists){
+//             return res.status(422).json({
+//                 error: "Please fill all fields properly"
+//             })
+//         }
+
+//         const User = new user({
+//             name, email, phone, work, password, cpassword
+//         })
+
+//         User.save().then(() => {
+//             res.status(201).json({
+//                 "message": "User registered successfully"
+//             })
+//         }).catch((err) => res.status(500).json({
+//             "error": "Registration failed"
+//         })); 
+            
+//     }).catch(err => {
+//         console.log("Error occured ", err)
+//     })
+
+// })
+
+// async-await
+
+router.post('/register', async (req, res) => {
 
     const { name, email, phone, work, password, cpassword } = req.body;
 
     if(!name || !email || !phone || !work || !password || !cpassword)
         return res.status(422).json({
             error: "Please fill all fields properly"
+    })
+
+    try{
+        const userExists = await user.findOne({
+            "email": email
         })
 
-    user.findOne({
-        "email": email
-    }).then((userExists) => {
-        if(userExists){
+        if(userExists) {
             return res.status(422).json({
-                error: "Please fill all fields properly"
+                error: "Email already exists"
             })
         }
 
         const User = new user({
             name, email, phone, work, password, cpassword
-        })
+        });
+    
+        await User.save()
 
-        User.save().then(() => {
-            res.status(201).json({
-                "message": "User registered successfully"
-            })
-        }).catch((err) => res.status(500).json({
-            "error": "Registration failed"
-        })); 
-            
-    }).catch(err => {
-        console.log("Error occured ", err)
-    })
+        res.status(201).json({
+            message: "User registered successfully"
+        })
+        
+    } catch(err) {
+        console.log("Error occured", err)
+    }
+
 
 })
 
